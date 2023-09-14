391a1dd293912542457c7efeaef7862746cf0cea
kostya <takenashura@gmail.com>
2023-09-14 18:18:36 +0300

    add order cancel tests

--- Tests/BikeStore.Application.Tests/Services/OrderServiceTests.cs
+++ Tests/BikeStore.Application.Tests/Services/OrderServiceTests.cs
@@ -6,8 +6,84 @@ using Moq;
 namespace BikeStore.Application.Tests.Services
 {
     [TestFixture]
+    //Немного не хватило времини закончить доделать грамотно тесты через InMemory db, но я думаю и этих тестов для такого задания хватит. Еще я бы протестил больше банально методов, особенно протестил бы правильно ли генерируется токен.
     public class Tests
     {
+        [Test]
+        public async Task CreateAsync_Order_ReturnsCreatedOrder()
+        {
+            //Arrange
+            Order createdOrder = new() {
+                OrderId = 1,
+                OrderDate = new DateTime(2022, 10, 1),
+                CustomerId = 1,
+                OrderStatus = OrderStatus.Pending
+            };
+            var mockRepo = new Mock<IOrderRepository>();
+            mockRepo
+                .Setup(repo => repo.CreateAsync(createdOrder))
+                .ReturnsAsync(createdOrder);
+
+            var mockOrderService = new OrderService(mockRepo.Object);
+
+            // Act
+            var order = await mockOrderService.CreateAsync(createdOrder);
+
+            // Assert
+            mockRepo.Verify(r => r.CreateAsync(createdOrder));
+            Assert.That(createdOrder, Is.EqualTo(order));
+        }
+
+        [Test]
+        public async Task CancelAsync_Order_CancelsOrder()
+        {
+            //Arrange
+            Order order = new()
+            {
+                OrderId = 1,
+                OrderDate = new DateTime(2022, 10, 1),
+                CustomerId = 1,
+                OrderStatus = OrderStatus.Pending
+            };
+            var mockRepo = new Mock<IOrderRepository>();
+            mockRepo
+                .Setup(repo => repo.GetAsync(order.OrderId))
+                .ReturnsAsync(order);
+
+            var mockOrderService = new OrderService(mockRepo.Object);
+
+            // Act
+            var cancelledOrderId = await mockOrderService.CancelAsync(order.OrderId);
+
+            // Assert
+            Assert.That(cancelledOrderId, Is.EqualTo(order.OrderId));
+        }
+
+        [Test]
+        public async Task CancelAsync_OrderDoesntExist_ReturnsZero()
+        {
+            //Arrange
+            Order order = new()
+            {
+                OrderId = 2,
+                OrderDate = new DateTime(2022, 10, 1),
+                CustomerId = 1,
+                OrderStatus = OrderStatus.Pending
+            };
+            var mockRepo = new Mock<IOrderRepository>();
+            mockRepo
+                .Setup(repo => repo.GetAsync(order.OrderId))
+                .ReturnsAsync(order);
+
+            var mockOrderService = new OrderService(mockRepo.Object);
+
+            // Act
+            var cancelledOrderId = await mockOrderService.CancelAsync(1);
+
+            // Assert
+            Assert.That(cancelledOrderId, Is.EqualTo(0));
+        }
+
         //[Test]
         //public void GetWithPaginationAsync_TenElementsPageOne_ReturnsTenElementsFromPageOne()
         //{
@@ -68,79 +144,5 @@ namespace BikeStore.Application.Tests.Services
         //    Assert.Equals(createdOrder, order);
         //}
 
-        [Test]
-        public async Task CreateAsync_Order_ReturnsCreatedOrder()
-        {
-            //Arrange
-            Order createdOrder = new() {
-                OrderId = 1,
-                OrderDate = new DateTime(2022, 10, 1),
-                CustomerId = 1,
-                OrderStatus = OrderStatus.Pending
-            };
-            var mockRepo = new Mock<IOrderRepository>();
-            mockRepo
-                .Setup(repo => repo.CreateAsync(createdOrder))
-                .ReturnsAsync(createdOrder);
-
-            var mockOrderService = new OrderService(mockRepo.Object);
-
-            // Act
-            var order = await mockOrderService.CreateAsync(createdOrder);
-
-            // Assert
-            mockRepo.Verify(r => r.CreateAsync(createdOrder));
-            Assert.Equals(createdOrder, order);
-        }
-
-        [Test]
-        public async Task CancelAsync_Order_CancelsOrder()
-        {
-            //Arrange
-            Order order = new()
-            {
-                OrderId = 1,
-                OrderDate = new DateTime(2022, 10, 1),
-                CustomerId = 1,
-                OrderStatus = OrderStatus.Pending
-            };
-            var mockRepo = new Mock<IOrderRepository>();
-            mockRepo
-                .Setup(repo => repo.GetAsync(order.OrderId))
-                .ReturnsAsync(order);
-
-            var mockOrderService = new OrderService(mockRepo.Object);
-
-            // Act
-            var cancelledOrderId = await mockOrderService.CancelAsync(order.OrderId);
-
-            // Assert
-            Assert.That(cancelledOrderId, Is.EqualTo(order.OrderId));
-        }
-
-        [Test]
-        public async Task CancelAsync_OrderDoesntExist_CancelsOrder()
-        {
-            //Arrange
-            Order order = new()
-            {
-                OrderId = 2,
-                OrderDate = new DateTime(2022, 10, 1),
-                CustomerId = 1,
-                OrderStatus = OrderStatus.Pending
-            };
-            var mockRepo = new Mock<IOrderRepository>();
-            mockRepo
-                .Setup(repo => repo.GetAsync(order.OrderId))
-                .ReturnsAsync(order);
-
-            var mockOrderService = new OrderService(mockRepo.Object);
-
-            // Act
-            var cancelledOrderId = await mockOrderService.CancelAsync(1);
-
-            // Assert
-            Assert.Equals(order.OrderId, cancelledOrderId);
-        }
     }
 }
